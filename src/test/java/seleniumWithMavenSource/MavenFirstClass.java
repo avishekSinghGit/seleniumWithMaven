8185c24dc10707be0bdfb9090f32fb11e6d2c126
Avishek Singh <avishekGithub@gmail.com>
2021-01-21 22:36:50 +0530

    Changes made in project

--- src/test/java/seleniumWithMavenSource/MavenFirstClass.java
+++ src/test/java/seleniumWithMavenSource/MavenFirstClass.java
@@ -1,18 +1,27 @@
 package seleniumWithMavenSource;
 
+import static org.testng.Assert.fail;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MavenFirstClass {
-	@Test
-	public void printMethod() {
+	@Test(groups= {"sanity"})
+	public void methodOne() {
 
 		System.out.println("First Maven Method =============>");
 	}
-	
-	@Test
-	public void printMethod1() {
 
-		System.out.println("First Maven Method1 =============>");
+	@Test(groups= {"sanity"})
+	public void methodTwo() {
+
+		System.out.println("Second Maven Method1 =============>");
 	}
 
+	@Test(groups= {"sanity"})
+	public void methodThree() {
+
+		System.out.println("Third Maven Method1 =============>");
+	}
 }
